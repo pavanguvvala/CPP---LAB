ad8073df6fc843a74b75fc374dc1a0fb1a4f644e
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-17 10:14:27 +0530

    Create program15.cpp

---
+++ program15.cpp
@@ -0,0 +1,22 @@
+// To demonstrate single inheritance in C++
+
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+    void eat() {
+        cout << "Animal is eating" << endl;
+    }
+};
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Dog is barking" << endl;
+    }
+};
+int main() {
+    Dog d;
+    d.eat();  // inherited function
+    d.bark(); // own function
+    return 0;
+}
