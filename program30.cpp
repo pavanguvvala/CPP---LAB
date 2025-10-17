29e376d6174999fa6c631741d5e367a825b42ce6
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-17 10:25:45 +0530

    Create program30.cpp

---
+++ program30.cpp
@@ -0,0 +1,16 @@
+// To demonstrate basic operations using deque (double-ended queue)
+
+#include <iostream>
+#include <deque>
+using namespace std;
+int main() {
+    deque<int> dq;
+    dq.push_back(10);
+    dq.push_front(5);
+    dq.push_back(15);
+    dq.pop_front(); // removes first element
+    cout << "Deque Elements: ";
+    for (int x : dq) cout << x << " ";
+    cout << endl;
+    return 0;
+}
