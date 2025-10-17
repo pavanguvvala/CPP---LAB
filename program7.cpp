2f0d085a00a478debb7988b86e92ce940863fbc0
pavanguvvala <pavanguvvala7@gmail.com>
2025-10-17 10:08:07 +0530

    Create program7.cpp

---
+++ program7.cpp
@@ -0,0 +1,25 @@
+// To demonstrate the use of public and private access specifiers in C++
+
+#include <iostream>
+using namespace std;
+class Student {
+private:
+    int rollNo;      // private
+    string name;     // private
+public:
+    void setData(int r, string n) {
+        rollNo = r;
+        name = n;
+    }
+        void display() {
+        cout << "Roll Number: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+    }
+};
+int main() {
+    Student s1;
+    s1.rollNo = 10;
+    s1.setData(10, "Alice"); 
+    s1.display();
+    return 0;
+}
